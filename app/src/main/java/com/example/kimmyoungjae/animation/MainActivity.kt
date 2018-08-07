286eae19c994a1c704c0da279b332fd93937db27
Kim-Myoung-jae <rlaaud30@naver.com>
2018-08-07 16:48:07 +0900

    add Handler for Delaying

--- app/src/main/java/com/example/kimmyoungjae/animation/MainActivity.kt
+++ app/src/main/java/com/example/kimmyoungjae/animation/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.kimmyoungjae.animation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,16 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn_click = findViewById(R.id.animation) as Button
+        val timer = Timer()
+
 
         btn_click.setOnClickListener {
-            Toast.makeText(this@MainActivity,"Clicked",Toast.LENGTH_LONG)
+            //Toast.makeText(this@MainActivity,"Clicked",Toast.LENGTH_LONG)
             av_from_code.setAnimation("rewards.json")
             av_from_code.playAnimation()
             av_from_code.loop(true)
+
+            Handler().postDelayed({
+                //doSomethingHere()
+                av_from_code.cancelAnimation()
+            }, 5000)
+
         }
-        /*av_from_code.setAnimation("rewards.json")
-        av_from_code.playAnimation()
-        av_from_code.loop(true)*/
+
+
     }
 
 
